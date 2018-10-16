07dbc929519d233c4011d70bea876cc4e6119ba1
Karl Benbeneck <kbenbeneck@gmail.com>
2018-10-16 02:21:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,15 @@
 # Your code goes here!
 class Dog
 
-  def initialize(name)
+  def name=(name)
     @name = name
   end
 
-  def name=(name)
+  def name
     @name
+  end
+
+  def bark
+    puts "Woof"
   end
 end
